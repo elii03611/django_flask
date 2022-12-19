951a09b5d3c12f309f27166f6bf9f322d6f513db
elii03611 <elii03611@gmail.com>
2022-12-19 16:20:51 +0200

    django_flask

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('home', views.index),
-    path('products', views.myProducts),
+    path('products', views.myProducts),     
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
--- base/views.py
+++ base/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render
 from .models import Product
 from django.http import JsonResponse
 from .serializer import ProductSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 def index(req):
     return JsonResponse('hello', safe=False)
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+ 
+ 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def myProducts(req):
     all_products = ProductSerializer(Product.objects.all(), many=True).data
     return JsonResponse(all_products, safe=False)
